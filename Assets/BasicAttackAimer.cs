3b3326a80675a9fe9657befa657889a6aae93e89
Jake Terpening <jaketerpening@gmail.com>
2022-04-11 09:34:33 -0700

    Player attack aiming added
    Moving how changes where the attack is placed
    Player 2 uses vertical attacks

---
+++ Assets/BasicAttackAimer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttackAimer : MonoBehaviour
+{
+    public enum AimAxis
+    {
+        horizontal,
+        vertical
+    }
+    public AimAxis myAxis;
+    public PlayerMovement movement;
+
+    public Transform attackTransform;
+
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    const float offset = 2.5f;
+    const float scaleLong = 3f;
+    const float scaleShort = 2f;
+    private void Update()
+    {
+        if (myAxis == AimAxis.horizontal)
+        {
+            if (Input.GetAxisRaw("Horizontal1") < 0)
+            {
+                attackTransform.localPosition = new Vector3(-offset, 0, 1);
+                attackTransform.localScale = new Vector3(-scaleLong, scaleShort, 1);
+            }
+            if (Input.GetAxisRaw("Horizontal1") > 0)
+            {
+                attackTransform.localPosition = new Vector3(offset, 0, 1);
+                attackTransform.localScale = new Vector3(scaleLong, scaleShort, 1);
+            }
+        }
+        else
+        {
+            if (Input.GetAxisRaw("Vertical2") < 0)
+            {
+                attackTransform.localPosition = new Vector3(0, -offset, 1);
+                attackTransform.localScale = new Vector3(-scaleLong, scaleShort, 1);
+            }
+            if (Input.GetAxisRaw("Vertical2") > 0)
+            {
+                attackTransform.localPosition = new Vector3(0, offset, 1);
+                attackTransform.localScale = new Vector3(scaleLong, scaleShort, 1);
+            }
+        }
+    }
+}
