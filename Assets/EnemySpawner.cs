ef5bfbf636b5020d0019d8e1917010843c148f2a
Jake Terpening <jaketerpening@gmail.com>
2022-04-11 17:14:45 -0700

    Enemy Spawner Updated

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,44 +4,97 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject[] enemyPrefabs;
+    public float[] weightsArray;    //keeps track of how much each unit is worth
     public float spawnInterval;
     public float lastSpawn;
 
     public float rampInterval;
     public float rampRatio;
+    public float rampScale;
     private float lastRamp;
 
-    public float xRange;
-    public float yRange;
+    public float northYVal;
+    public float northXMin;
+    public float northXMax;
+
+    public float southYVal;
+    public float southXMin;
+    public float southXMax;
+
+    public float westXVal;
+    public float westYMin;
+    public float westYMax;
+
+    public float eastXVal;
+    public float eastYMin;
+    public float eastYMax;
+
+    public int currentLevel;
+    private int maxLevel;
+
+    public float currentWeight = 5f;
+    private float remainder = 0f;
 
     private void Awake()
     {
-        //lastSpawn = Time.time;
+        maxLevel = enemyPrefabs.Length;
     }
     private void Update()
     {
         if (Time.time > spawnInterval + lastSpawn)
         {
-            SpawnEnemy(spawnPostion());
+            SpawnGroup(currentWeight, currentLevel);
             lastSpawn = Time.time;
         }
         if(Time.time>rampInterval+lastRamp)
         {
-            spawnInterval *= rampRatio;
+            currentWeight += rampScale;
             lastRamp = Time.time;
         }
     }
 
     Vector3 spawnPostion()
     {
-        Vector3 center = transform.position;
-        float xPos = Random.Range(center.x - xRange, center.x + xRange);
-        float yPos = Random.Range(center.y - yRange, center.y + yRange);
-        return new Vector3(xPos, yPos);
+        int zone = Random.Range(0, 4);
+        float xPos = 0;
+        float yPos = 0;
+        switch (zone)
+        {
+            case 0:
+                xPos = Random.Range(northXMin, northXMax);
+                yPos = northYVal;
+                return new Vector3(xPos, yPos);
+            case 1:
+                xPos = eastXVal;
+                yPos = Random.Range(eastYMin, eastYMax);
+                return new Vector3(xPos, yPos);
+            case 2:
+                xPos = Random.Range(southXMin, southXMax);
+                yPos = southYVal;
+                return new Vector3(xPos, yPos);
+            default:
+                xPos = westXVal;
+                yPos = Random.Range(westYMin, westYMax);
+                return new Vector3(xPos, yPos);
+
+        }
     }
-    void SpawnEnemy(Vector3 position)
+
+    void SpawnGroup(float weight, int strongest)
     {
-        Instantiate(enemyPrefab, position, Quaternion.identity);
+        weight = weight + remainder;
+        while(weight > 0)
+        {
+            int unit = Random.Range(0, strongest);
+            float unitWeight = weightsArray[unit];
+            weight -= unitWeight;
+            SpawnEnemy(spawnPostion(), unit);
+        }
+        remainder = weight;
+    }
+    void SpawnEnemy(Vector3 position, int index = 0)
+    {
+        Instantiate(enemyPrefabs[index], position, Quaternion.identity);
     }
 }
